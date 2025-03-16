127986911fcf970f0a9b35ef9ed2d3261f3993a0
Jangidtarun <tarundjangid@gmail.com>
2025-03-16 15:52:32 +0530

    boid can now move; next up -> target following

--- main.c
+++ main.c
@@ -10,7 +10,68 @@
 #define GREEN 0, 255, 0, 255
 #define BLUE 0, 0, 255, 255
 
-#define FRAME_DELAY 1000 / 25
+#define FRAME_DELAY 1000 / 60
+
+// Boid
+typedef struct {
+		// position
+		double x, y;
+		
+		// velocity
+		double vx, vy;
+
+		// acceleration
+		double ax, ay;
+} boid_t;
+
+
+// this function is used for drawing the boid
+void draw_boid(const boid_t *boid, SDL_Renderer *renderer) {
+		// draw velocity
+		SDL_SetRenderDrawColor(renderer, RED);
+		SDL_RenderDrawLine(renderer, boid->x, boid->y, boid->x + boid->vx, boid->y + boid->vy);
+
+		// draw acceleration
+		SDL_SetRenderDrawColor(renderer, BLUE);
+		SDL_RenderDrawLine(renderer, boid->x, boid->y, boid->x + boid->ax, boid->y + boid->ay);
+		
+		// draw the point
+		SDL_SetRenderDrawColor(renderer, GREEN);
+		SDL_RenderDrawPoint(renderer, boid->x, boid->y);
+}
+
+
+// this function is used for creating the boid
+boid_t *create_boid_dynamic(double x, double y, double vx, double vy, double ax, double ay) {
+		boid_t *boid = (boid_t *)malloc(sizeof(boid_t));
+
+		if(!boid) {
+				fprintf(stderr, "Memory allocation failed\n");
+				exit(1);
+		}
+
+		boid->x = x;
+		boid->y = y;
+		boid->vx = vx;
+		boid->vy = vy;
+		boid->ax = ax;
+		boid->ay = ay;
+
+		return boid;
+}
+
+
+// this function is used for updating the boid
+void update_boid(boid_t *boid, const double dt) {
+		// update the position
+		boid->x += boid->vx * dt;
+		boid->y += boid->vy * dt;
+
+		// update the velocity
+		boid->vx += boid->ax * dt;
+		boid->vy += boid->ay * dt;
+}
+
 
 int main(int argc, char **argv) {
 		printf("starting...\n");
@@ -38,15 +99,21 @@ int main(int argc, char **argv) {
 
 		int running = 1;
 		SDL_Event event;
-		int x = 400, y = 300;
+		// int x = 400, y = 300;
+		
+		// create a boid
+		boid_t *boid = create_boid_dynamic(400, 300, 40, -20, 10, 20);
+
+		// store initial time
+		int start_time = SDL_GetTicks();
 
 		while(running) {
 				while(SDL_PollEvent(&event)) {
 						if(event.type == SDL_QUIT) {
 								running = 0;
 						} else if(event.type == SDL_MOUSEMOTION) {
-								x = event.motion.x;
-								y = event.motion.y;
+								// x = event.motion.x;
+								// y = event.motion.y;
 						}
 				}
 
@@ -55,8 +122,17 @@ int main(int argc, char **argv) {
 				SDL_RenderClear(renderer);
 
 				// set draw color
-				SDL_SetRenderDrawColor(renderer, GREEN);
-				SDL_RenderDrawPoint(renderer, x, y);
+				// SDL_SetRenderDrawColor(renderer, GREEN);
+				// SDL_RenderDrawPoint(renderer, x, y);
+				
+				int current_time = SDL_GetTicks();
+				int dt_ms = current_time - start_time;
+				start_time = current_time;
+
+				double dt = dt_ms / 1000.0f;
+
+				draw_boid(boid, renderer);
+				update_boid(boid, dt);
 
 				SDL_RenderPresent(renderer);
 				SDL_Delay(FRAME_DELAY);
@@ -64,6 +140,9 @@ int main(int argc, char **argv) {
 		}
 
 		printf("exiting...\n");
+
+		// free memory if allocated
+		free(boid);
 
 		SDL_DestroyWindow(window);
 		SDL_Quit();
